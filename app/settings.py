eee195a23d2f001671fa5b7f715ba51f6d193eab
andrew-zolin <zolin.andrey333@gmail.com>
2025-10-05 15:19:57 +0500

    update corse origins

--- app/settings.py
+++ app/settings.py
@@ -54,7 +54,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://62.113.58.80:5757", 
     "http://62.113.58.80:5656",
-    "https://admin.lovinad.com/" 
+    "https://admin.lovinad.com" 
 ]
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_METHODS = [
